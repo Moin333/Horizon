5898dd18733efd6fc52b812070d63bb8884acc52
Moin333 <ansarimoin7861@gmail.com>
2025-08-05 12:35:32 +0530

    Final Commit v1.0

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -12,6 +12,7 @@ import Image from "next/image"
 import Link from "next/link"
 import { usePathname } from "next/navigation"
 import Footer from "./Footer"
+import PlaidLink from "./PlaidLink"
 
 const MobileNav = ({ user }: MobileNavProps) => {
   const pathname = usePathname();
@@ -66,7 +67,7 @@ const MobileNav = ({ user }: MobileNavProps) => {
                 )
               })}
 
-              USER
+                <PlaidLink user={user}/>
               </nav>
             </SheetClose>
 
